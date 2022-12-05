07c4c2bb77d89d2c4f3ba4b0712ad19f47d23d46
dennisobel <ogembodenis2016@gmail.com>
2022-12-05 10:20:44 +0300

    initial commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "ab2e9330821c4b44fbb1ed81835c0ed7",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
